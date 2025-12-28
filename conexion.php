efa18e8fb3c9548f80ef97f951d178e2afc10b20
FerjoelGutierrez <163078576+FerjoelGutierrez@users.noreply.github.com>
2025-12-27 21:50:48 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,17 @@
 <?php
-// Configuración Supabase Vitapro 🛡️
+// Configuración Supabase Vitapro 🛡️ (Repositiorio Privado - Seguro)
 $host = 'db.piliozcqxjhcpeewpicx.supabase.co'; 
 $port = '5432';
 $dbname = 'postgres';
 $user = 'postgres'; 
-$password = 'TU_CONTRASEÑA_DE_SUPABASE'; // La que elegiste al crear el proyecto
+$password = 'nIcovita2025'; 
 try {
-    // Usamos el driver pgsql para PostgreSQL
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
-    die("Error crítico de conexión: " . $e->getMessage());
+    die("Error crítico de conexión.");
 }
 ?>
